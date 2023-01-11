3ac3e178b0655135be1f02f9b6b4f3d0680fdff1
Tiago Jacinto <tiagojacintodev@gmail.com>
2023-01-11 15:58:02 +0000

    Define string formatter helper functions

---
+++ src/helpers/functions.ts
@@ -0,0 +1,27 @@
+export const indentText = (text: string, indentation: number) => {
+  return " ".repeat(indentation) + text;
+};
+
+export const paragraph = (text: string) => "\n" + text;
+
+export const formatBody = (body: string, indentation: number) => {
+  const multiplier = 3;
+  const lines = body.split(/\n/);
+
+  const indentedLines = lines.map((line, index) => {
+    const lastIndex = lines.length - 1;
+    let newLine = indentText(`"${line}"`, indentation * multiplier);
+
+    if (index !== 0) {
+      newLine = paragraph(newLine);
+    }
+
+    if (index !== lastIndex) {
+      newLine += ",";
+    }
+
+    return newLine;
+  });
+
+  return indentedLines.join("");
+};
